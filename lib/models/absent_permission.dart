61a4ada04c8bffa59bd049413995507cd5643f71
ryanaidilp <fajrianaidilp@gmail.com>
2020-12-06 19:14:07 +0800

    ref : refactor user model and absent permission model

--- lib/models/absent_permission.dart
+++ lib/models/absent_permission.dart
@@ -1,27 +1,34 @@
+import 'package:spo_balaesang/models/user.dart';
+
 class AbsentPermission {
   const AbsentPermission(
-      {this.title,
+      {this.id,
+      this.title,
       this.dueDate,
       this.startDate,
       this.description,
       this.photo,
-      this.isApproved});
+      this.isApproved,
+      this.user});
 
+  final int id;
   final String title;
   final String description;
   final bool isApproved;
   final String photo;
   final DateTime dueDate;
   final DateTime startDate;
+  final User user;
 
   factory AbsentPermission.fromJson(Map<String, dynamic> json) {
     return AbsentPermission(
-      title: json['title'] as String,
-      description: json['description'] as String,
-      isApproved: json['is_approved'] as bool,
-      photo: json['photo'] as String,
-      dueDate: DateTime.parse(json['due_date']),
-      startDate: DateTime.parse(json['start_date']),
-    );
+        id: json['id'] as int,
+        title: json['title'] as String,
+        description: json['description'] as String,
+        isApproved: json['is_approved'] as bool,
+        photo: json['photo'] as String,
+        dueDate: DateTime.parse(json['due_date']),
+        startDate: DateTime.parse(json['start_date']),
+        user: json['user'] != null ? User.fromJson(json['user']) : null);
   }
 }
