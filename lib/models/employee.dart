9024a8f6d50d93c7ae9cb2ed51fdcb9b7e2945bb
ryanaidilp <fajrianaidilp@gmail.com>
2020-12-10 13:09:28 +0800

    ref : add presence to employee model

--- lib/models/employee.dart
+++ lib/models/employee.dart
@@ -1,13 +1,15 @@
+import 'package:spo_balaesang/models/presence.dart';
+
 class Employee {
-  const Employee({
-    this.nip,
-    this.name,
-    this.phone,
-    this.gender,
-    this.department,
-    this.status,
-    this.position,
-  });
+  const Employee(
+      {this.nip,
+      this.name,
+      this.phone,
+      this.gender,
+      this.department,
+      this.status,
+      this.position,
+      this.presences});
 
   final String nip;
   final String name;
@@ -16,16 +18,21 @@ class Employee {
   final String department;
   final String status;
   final String position;
+  final List<Presence> presences;
 
   factory Employee.fromJson(Map<String, dynamic> json) {
     return Employee(
-      nip: json['nip'] as String,
-      name: json['name'] as String,
-      phone: json['phone'] as String,
-      gender: json['gender'] as String,
-      department: json['department'] as String,
-      status: json['status'] as String,
-      position: json['position'] as String,
-    );
+        nip: json['nip'] as String,
+        name: json['name'] as String,
+        phone: json['phone'] as String,
+        gender: json['gender'] as String,
+        department: json['department'] as String,
+        status: json['status'] as String,
+        position: json['position'] as String,
+        presences: json['presence'] != null
+            ? (json['presence'] as List<dynamic>)
+                .map((json) => Presence.fromJson(json))
+                .toList()
+            : null);
   }
 }
