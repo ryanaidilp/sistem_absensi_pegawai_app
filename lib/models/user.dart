af6c96763f3679f5ad3bbb62b594e74f996377fe
ryanaidilp <fajrianaidilp@gmail.com>
2020-12-10 13:10:03 +0800

    ref : add notifications, unreadNotifications, holiday, and isWeekend properties to user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -10,9 +10,12 @@ class User {
       this.department,
       this.status,
       this.position,
+      this.unreadNotification,
       this.token,
       this.nextPresence,
-      this.presences});
+      this.presences,
+      this.holiday,
+      this.isWeekend});
 
   final int id;
   final String nip;
@@ -22,20 +25,26 @@ class User {
   final String department;
   final String status;
   final String position;
+  final int unreadNotification;
   final String token;
   final Presence nextPresence;
   final List<Presence> presences;
+  final Map<String, dynamic> holiday;
+  final bool isWeekend;
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
         id: json['id'] as int,
         nip: json['nip'] as String,
         name: json['name'] as String,
-        phone: json['phonr'] as String,
+        phone: json['phone'] as String,
         gender: json['gender'] as String,
         department: json['department'] as String,
         status: json['status'] as String,
         position: json['position'] as String,
+        unreadNotification: json['unread_notifications'] as int,
+        holiday: json['holiday'] as Map<String, dynamic>,
+        isWeekend: json['is_weekend'] as bool,
         token: json['token'] as String,
         nextPresence: json['next_presence'] != null
             ? Presence.fromJson(
