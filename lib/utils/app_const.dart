386e71829924e123f99ec91bd5343865ccbf07e7
ryanaidilp <fajrianaidilp@gmail.com>
2020-12-30 03:31:32 +0800

    chore : extract all model string field & shared preferences keys to app_const.dart

---
+++ lib/utils/app_const.dart
@@ -0,0 +1,66 @@
+// Shared Preferences Keys
+const String PREFS_TOKEN_KEY = 'token';
+const String PREFS_USER_KEY = 'user';
+const String PREFS_EMPLOYEE_KEY = 'employee';
+const String PREFS_ALARM_KEY = 'alarm';
+const String PREFS_SEEN_KEY = 'seen';
+
+// General
+const String JSON_DATA_FIELD = 'data';
+
+// User/Employee
+const String USER_ID_FIELD = 'id';
+const String USER_NIP_FIELD = 'nip';
+const String USER_NAME_FIELD = 'name';
+const String USER_PHONE_FIELD = 'phone';
+const String USER_GENDER_FIELD = 'gender';
+const String USER_DEPARTMENT_FIELD = 'department';
+const String USER_STATUS_FIELD = 'status';
+const String USER_POSITION_FIELD = 'position';
+const String USER_UNREAD_NOTIFICATION_COUNT_FIELD = 'unread_notifications';
+const String USER_HOLIDAY_FIELD = 'holiday';
+const String USER_IS_WEEKEND_FIELD = 'is_weekend';
+const String USER_TOKEN_FIELD = 'token';
+const String USER_NEXT_PRESENCE_FIELD = 'next_presence';
+const String USER_PRESENCES_FIELD = 'presence';
+
+// Presence
+const String PRESENCE_DATE_FIELD = 'date';
+const String PRESENCE_CODE_TYPE_FIELD = 'code_type';
+const String PRESENCE_STATUS_FIELD = 'status';
+const String PRESENCE_ATTEND_TIME_FIELD = 'attend_time';
+const String PRESENCE_LOCATION_FIELD = 'location';
+const String PRESENCE_PHOTO_FIELD = 'photo';
+const String PRESENCE_START_TIME_FIELD = 'start_time';
+const String PRESENCE_END_TIME_FIELD = 'end_time';
+
+// Outstation/Permission
+const String OUTSTATION_ID_FIELD = 'id';
+const String OUTSTATION_TITLE_FIELD = 'title';
+const String OUTSTATION_DESCRIPTION_FIELD = 'description';
+const String OUTSTATION_IS_APPROVED_FIELD = 'is_approved';
+const String OUTSTATION_PHOTO_FIELD = 'photo';
+const String OUTSTATION_DUE_DATE_FIELD = 'due_date';
+const String OUTSTATION_START_DATE_FIELD = 'start_date';
+const String OUTSTATION_USER_FIELD = 'user';
+
+// Absent Permission
+const String ABSENT_PERMISSION_ID_FIELD = 'id';
+const String ABSENT_PERMISSION_TITLE_FIELD = 'title';
+const String ABSENT_PERMISSION_DESCRIPTION_FIELD = 'description';
+const String ABSENT_PERMISSION_IS_APPROVED_FIELD = 'is_approved';
+const String ABSENT_PERMISSION_PHOTO_FIELD = 'photo';
+const String ABSENT_PERMISSION_DUE_DATE_FIELD = 'due_date';
+const String ABSENT_PERMISSION_START_DATE_FIELD = 'start_date';
+const String ABSENT_PERMISSION_USER_FIELD = 'user';
+
+// Notification
+const String NOTIFICATION_ID_FIELD = 'id';
+const String NOTIFICATION_NOTIFIABLE_ID_FIELD = 'notifiable_id';
+const String NOTIFICATION_NOTIFIABLE_TYPE_FIELD = 'notifiable_type';
+const String NOTIFICATION_IS_READ_FIELD = 'is_read';
+
+// Location
+const String LOCATION_LATITUDE_FIELD = 'latitude';
+const String LOCATION_LONGITUDE_FIELD = 'longitude';
+const String LOCATION_ADDRESS_FIELD = 'address';
