e43dac8f591d1e2376a36b61059fb3f9378d960b
ryanaidilp <fajrianaidilp@gmail.com>
2021-01-10 19:39:34 +0800

    update : update create_outstation_screen.dart & create_permission_screen.dart - replace showDatePicker with TableCalendar - check if date is updated to make sure user's set startDate and dueDate

--- lib/screen/create_permission_screen.dart
+++ lib/screen/create_permission_screen.dart
@@ -11,8 +11,10 @@ import 'package:progress_dialog/progress_dialog.dart';
 import 'package:provider/provider.dart';
 import 'package:spo_balaesang/repositories/data_repository.dart';
 import 'package:spo_balaesang/screen/bottom_nav_screen.dart';
+import 'package:spo_balaesang/screen/image_detail_screen.dart';
 import 'package:spo_balaesang/utils/file_util.dart';
 import 'package:spo_balaesang/utils/view_util.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CreatePermissionScreen extends StatefulWidget {
   @override
@@ -25,6 +27,10 @@ class _CreatePermissionScreenState extends State<CreatePermissionScreen> {
   File _tmpFile;
   DateTime _dueDate = DateTime.now();
   DateTime _startDate = DateTime.now();
+  bool _isDateChange = false;
+
+  final CalendarController _startDateController = CalendarController();
+  final CalendarController _dueDateController = CalendarController();
 
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
@@ -41,30 +47,35 @@ class _CreatePermissionScreenState extends State<CreatePermissionScreen> {
   }
 
   Future<void> _uploadData() async {
-    ProgressDialog pd = ProgressDialog(context, isDismissible: false);
-    try {
-      pd.show();
-      final dataRepo = Provider.of<DataRepository>(context, listen: false);
-      Map<String, dynamic> data = {
-        'title': _titleController.value.text,
-        'description': _descriptionController.value.text,
-        'photo': _base64Image,
-        'due_date': _dueDate.toIso8601String(),
-        'start_date': _startDate.toIso8601String(),
-        'file_name': _fileName
-      };
-      Map<String, dynamic> _res = await dataRepo.permission(data);
-      if (_res['success']) {
+    if (!_isDateChange) {
+      showAlertDialog(
+          'failed', 'Pelanggaran', 'Pilih tanggal terlebih dahulu!', true);
+    } else {
+      ProgressDialog pd = ProgressDialog(context, isDismissible: false);
+      try {
+        pd.show();
+        final dataRepo = Provider.of<DataRepository>(context, listen: false);
+        Map<String, dynamic> data = {
+          'title': _titleController.value.text,
+          'description': _descriptionController.value.text,
+          'photo': _base64Image,
+          'due_date': _dueDate.toIso8601String(),
+          'start_date': _startDate.toIso8601String(),
+          'file_name': _fileName
+        };
+        Map<String, dynamic> _res = await dataRepo.permission(data);
+        if (_res['success']) {
+          pd.hide();
+          showAlertDialog('success', "Sukses", _res['message'], false);
+          Timer(Duration(seconds: 5), () => Get.off(BottomNavScreen()));
+        } else {
+          if (pd.isShowing()) pd.hide();
+          showErrorDialog(_res);
+        }
+      } catch (e) {
+        print(e.toString());
         pd.hide();
-        showAlertDialog('success', "Sukses", _res['message'], false);
-        Timer(Duration(seconds: 5), () => Get.off(BottomNavScreen()));
-      } else {
-        if (pd.isShowing()) pd.hide();
-        showErrorDialog(_res);
       }
-    } catch (e) {
-      print(e.toString());
-      pd.hide();
     }
   }
 
@@ -76,37 +87,111 @@ class _CreatePermissionScreenState extends State<CreatePermissionScreen> {
     }
 
     Uint8List bytes = base64Decode(_base64Image);
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10.0),
-      child: Image.memory(bytes),
+    return InkWell(
+      onTap: () {
+        Get.to(ImageDetailScreen(
+          bytes: bytes,
+          tag: 'image',
+        ));
+      },
+      child: Hero(
+        tag: 'image',
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10.0),
+          child: Image.memory(
+            bytes,
+            width: Get.width,
+            height: 250,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
     );
   }
 
-  _selectDueDate(BuildContext context) async {
-    final DateTime picked = await showDatePicker(
-        context: context,
-        initialDate: _dueDate,
-        firstDate: DateTime.now().subtract(Duration(days: 7)),
-        lastDate: DateTime(DateTime.now().year + 5));
-    if (picked != null) {
-      setState(() {
-        _dueDate = picked;
-      });
-    }
+  _selectDueDate() async {
+    Get.defaultDialog(
+        title: 'Pilih Tanggal Selesai',
+        content: Flexible(
+          child: Container(
+            width: Get.width * 0.9,
+            child: TableCalendar(
+              availableCalendarFormats: <CalendarFormat, String>{
+                CalendarFormat.month: '1 minggu',
+                CalendarFormat.twoWeeks: '1 bulan',
+                CalendarFormat.week: '2 minggu'
+              },
+              availableGestures: AvailableGestures.horizontalSwipe,
+              headerStyle:
+                  HeaderStyle(formatButtonTextStyle: TextStyle(fontSize: 12.0)),
+              calendarController: _dueDateController,
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              startDay: DateTime.now().subtract(Duration(days: 7)),
+              initialSelectedDay: _dueDate,
+              locale: 'in_ID',
+              initialCalendarFormat: CalendarFormat.month,
+              onDaySelected: (day, events, holidays) {
+                Get.back();
+                setState(() {
+                  if (!_isDateChange) {
+                    _isDateChange = true;
+                  }
+                  _dueDate = day;
+                  if (_dueDate.isBefore(_startDate)) {
+                    _startDate = day;
+                  }
+                });
+              },
+            ),
+          ),
+        ));
   }
 
-  _selectStartDate(BuildContext context) async {
-    final DateTime picked = await showDatePicker(
-      context: context,
-      initialDate: _startDate,
-      firstDate: DateTime.now().subtract(Duration(days: 7)),
-      lastDate: DateTime(DateTime.now().year + 5),
-    );
-    if (picked != null) {
-      setState(() {
-        _startDate = picked;
-      });
-    }
+  _selectStartDate() async {
+    Get.defaultDialog(
+        title: 'Pilih Tanggal Mulai',
+        content: Flexible(
+          child: Container(
+            width: Get.width * 0.9,
+            child: TableCalendar(
+              availableCalendarFormats: <CalendarFormat, String>{
+                CalendarFormat.month: '1 minggu',
+                CalendarFormat.twoWeeks: '1 bulan',
+                CalendarFormat.week: '2 minggu'
+              },
+              availableGestures: AvailableGestures.horizontalSwipe,
+              headerStyle:
+                  HeaderStyle(formatButtonTextStyle: TextStyle(fontSize: 12.0)),
+              calendarController: _startDateController,
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              startDay: DateTime.now().subtract(Duration(days: 7)),
+              initialSelectedDay: _startDate,
+              locale: 'in_ID',
+              initialCalendarFormat: CalendarFormat.month,
+              onDaySelected: (day, events, holidays) {
+                Get.back();
+                setState(() {
+                  if (!_isDateChange) {
+                    _isDateChange = true;
+                  }
+                  _startDate = day;
+                  if (_startDate.isAfter(_dueDate)) {
+                    _dueDate = day;
+                  }
+                });
+              },
+            ),
+          ),
+        ));
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descriptionController.dispose();
+    _startDateController.dispose();
+    _dueDateController.dispose();
+    super.dispose();
   }
 
   @override
@@ -142,7 +227,7 @@ class _CreatePermissionScreenState extends State<CreatePermissionScreen> {
                 ],
               ),
               Text(
-                'Mis: Izin Sakit, Izin Kegiatan Lapangan, dsb.',
+                'Mis: Izin Sakit, dsb.',
                 style: TextStyle(color: Colors.grey),
               ),
               TextFormField(
@@ -204,9 +289,7 @@ class _CreatePermissionScreenState extends State<CreatePermissionScreen> {
                       '${DateFormat('EEEE, d MMMM y').format(
                         _startDate,
                       )}',
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
+                      style: TextStyle(fontWeight: FontWeight.w600),
                     ),
                   ),
                   IconButton(
@@ -215,7 +298,7 @@ class _CreatePermissionScreenState extends State<CreatePermissionScreen> {
                         color: Colors.blueAccent,
                       ),
                       onPressed: () {
-                        _selectStartDate(context);
+                        _selectStartDate();
                       })
                 ],
               ),
@@ -241,9 +324,7 @@ class _CreatePermissionScreenState extends State<CreatePermissionScreen> {
                       '${DateFormat('EEEE, d MMMM y').format(
                         _dueDate,
                       )}',
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
+                      style: TextStyle(fontWeight: FontWeight.w600),
                     ),
                   ),
                   IconButton(
@@ -252,7 +333,7 @@ class _CreatePermissionScreenState extends State<CreatePermissionScreen> {
                         color: Colors.blueAccent,
                       ),
                       onPressed: () {
-                        _selectDueDate(context);
+                        _selectDueDate();
                       })
                 ],
               ),
@@ -270,6 +351,13 @@ class _CreatePermissionScreenState extends State<CreatePermissionScreen> {
               Text(
                 'Lampirkan foto surat izin atau surat lainnya seperti surat keterangan sakit dsb.',
                 style: TextStyle(color: Colors.grey),
+              ),
+              Text(
+                '*tekan untuk memperbesar',
+                style: TextStyle(
+                    fontSize: 12.0,
+                    color: Colors.black87,
+                    fontStyle: FontStyle.italic),
               ),
               SizedBox(height: 20.0),
               _showImage(),
