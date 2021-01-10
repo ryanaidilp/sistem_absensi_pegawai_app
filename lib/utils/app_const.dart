f17ce103e6a50fa9b6cce3ee98130216f62b196d
ryanaidilp <fajrianaidilp@gmail.com>
2021-01-10 19:19:30 +0800

    chore : add new const in app_const.dart - add paidLeaveCategories - add new field const for report model - add field const for paid leave model - add rank & group field const for user model

--- lib/utils/app_const.dart
+++ lib/utils/app_const.dart
@@ -24,6 +24,8 @@ const String USER_IS_WEEKEND_FIELD = 'is_weekend';
 const String USER_TOKEN_FIELD = 'token';
 const String USER_NEXT_PRESENCE_FIELD = 'next_presence';
 const String USER_PRESENCES_FIELD = 'presence';
+const String USER_RANK_FIELD = 'rank';
+const String USER_GROUP_FIELD = 'group';
 
 // Presence
 const String PRESENCE_DATE_FIELD = 'date';
@@ -44,6 +46,17 @@ const String OUTSTATION_PHOTO_FIELD = 'photo';
 const String OUTSTATION_DUE_DATE_FIELD = 'due_date';
 const String OUTSTATION_START_DATE_FIELD = 'start_date';
 const String OUTSTATION_USER_FIELD = 'user';
+
+// Paid Leave
+const String PAID_LEAVE_ID_FIELD = 'id';
+const String PAID_LEAVE_TITLE_FIELD = 'title';
+const String PAID_LEAVE_CATEGORY_FIELD = 'category';
+const String PAID_LEAVE_DESCRIPTION_FIELD = 'description';
+const String PAID_LEAVE_IS_APPROVED_FIELD = 'is_approved';
+const String PAID_LEAVE_PHOTO_FIELD = 'photo';
+const String PAID_LEAVE_DUE_DATE_FIELD = 'due_date';
+const String PAID_LEAVE_START_DATE_FIELD = 'start_date';
+const String PAID_LEAVE_USER_FIELD = 'user';
 
 // Absent Permission
 const String ABSENT_PERMISSION_ID_FIELD = 'id';
@@ -69,7 +82,16 @@ const String LOCATION_ADDRESS_FIELD = 'address';
 // Absent Report
 const String REPORT_ATTENDANCE_PERCENTAGE_FIELD = 'attendance_percentage';
 const String REPORT_DAY_FIELD = 'day';
+const String REPORT_LIMIT_FIELD = 'limit';
 const String REPORT_LATE_COUNT_FIELD = 'late_count';
+const String REPORT_LEAVE_EARLY_COUNT_FIELD = 'leave_early_count';
+const String REPORT_NOT_MORNING_PARADE_COUNT_FIELD = 'not_morning_parade_count';
+const String REPORT_TOTAL_WORK_DAY_FIELD = 'total_work_day';
+const String REPORT_ANNUAL_LEAVE_FIELD = 'annual_leave';
+const String REPORT_IMPORTANT_REASON_LEAVE_FIELD = 'important_reason_leave';
+const String REPORT_SICK_LEAVE_FIELD = 'sick_leave';
+const String REPORT_MATERNITY_LEAVE_FIELD = 'maternity_leave';
+const String REPORT_OUT_OF_LIABILITY_LEAVE_FIELD = 'out_of_liability_leave';
 const String REPORT_PERCENTAGE_FIELD = 'percentage';
 const String ABSENT_REPORT_YEARLY_FIELD = 'yearly';
 const String ABSENT_REPORT_MONTHLY_FIELD = 'monthly';
@@ -92,3 +114,11 @@ const String YEARLY_OUTSTATION_FIELD = 'outstation';
 const String HOLIDAY_DATE_FIELD = 'date';
 const String HOLIDAY_NAME_FIELD = 'name';
 const String HOLIDAY_DESCRIPTION_FIELD = 'description';
+
+const Map<String, dynamic> paidLeaveCategories = {
+  'Cuti Tahunan (97.5%)': 1,
+  'Cuti Alasan Penting (97.5%)': 2,
+  'Cuti Bersalin (97.5%)': 3,
+  'Cuti Sakit (97.5%)': 4,
+  'Cuti Diluar Tanggungan (0%)': 5
+};
