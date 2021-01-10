886858ebe08bedbb20087dfd2fb3d442e6678424
ryanaidilp <fajrianaidilp@gmail.com>
2021-01-10 19:21:44 +0800

    chore : add & extract some view utility function - calculateLateInMinutes (String of late in minutes) - formatPercentage - formatCurrency - add new category for checkPresencePercentage & checkColorStatus

--- lib/utils/view_util.dart
+++ lib/utils/view_util.dart
@@ -1,6 +1,7 @@
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
 Future showAlertDialog(
@@ -103,9 +104,34 @@ Color checkStatusColor(String status) {
       return Colors.orange;
     case 'Dinas Luar':
     case 'Izin':
-      return Colors.blue;
+      return Colors.blueAccent;
+    case 'Cuti Tahunan':
+    case 'Cuti Bersalin':
+    case 'Cuti Sakit':
+    case 'Cuti Alasan Penting':
+      return Colors.pink;
     default:
       return Colors.red;
+  }
+}
+
+double checkPresencePercentage(String status) {
+  switch (status) {
+    case 'Tepat Waktu':
+    case 'Dinas Luar':
+    case 'Cuti Tahunan':
+      return 100;
+    case 'Cuti Bersalin':
+    case 'Cuti Sakit':
+    case 'Cuti Alasan Penting':
+      return 97.5;
+    case 'Terlambat':
+      return 25;
+      break;
+    case 'Izin':
+      return 50;
+    default:
+      return 0;
   }
 }
 
@@ -131,8 +157,8 @@ List<PageViewModel> onBoardingScreens = [
             TextStyle(fontSize: 12.0, fontWeight: FontWeight.normal)),
   ),
   PageViewModel(
-    title: "Izin Absen & Dinas Luar",
-    body: "Ajukan izin absen dan dinas luar melalui aplikasi.",
+    title: "Izin, Cuti, & Dinas Luar",
+    body: "Ajukan Izin, Cuti, dan Dinas Luar melalui aplikasi.",
     image: Center(
         child: Container(
       child: FlareActor(
@@ -151,3 +177,23 @@ List<PageViewModel> onBoardingScreens = [
             TextStyle(fontSize: 12.0, fontWeight: FontWeight.normal)),
   )
 ];
+
+String calculateLateInMinutes(DateTime startTime, String attendTime) {
+  var attendDate = startTime.year.toString() +
+      '-' +
+      startTime.month.toString().padLeft(2, '0') +
+      '-' +
+      startTime.day.toString().padLeft(2, '0');
+  var duration =
+      DateTime.parse('$attendDate $attendTime').difference(startTime).inMinutes;
+
+  return '$duration menit';
+}
+
+String formatPercentage(double percentage) {
+  return '${NumberFormat.decimalPattern('id_ID').format(percentage)}%';
+}
+
+String formatCurrency(double salary) {
+  return NumberFormat.currency(locale: 'id_ID', symbol: 'Rp. ').format(salary);
+}
