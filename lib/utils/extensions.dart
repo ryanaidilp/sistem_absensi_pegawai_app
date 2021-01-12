00bce46553d7290c8e3a73d850648c25b9c749f8
ryanaidilp <fajrianaidilp@gmail.com>
2021-01-12 10:26:34 +0800

    ref : extract extension to extensions.dart

---
+++ lib/utils/extensions.dart
@@ -0,0 +1,7 @@
+extension DateOnlyCompare on DateTime {
+  bool isSameDate(DateTime other) {
+    return this.year == other.year &&
+        this.month == other.month &&
+        this.day == other.day;
+  }
+}
