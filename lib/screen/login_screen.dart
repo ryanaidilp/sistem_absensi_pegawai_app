de4845e459d82f23ce034bea0478bca7ba64b2a5
ryanaidilp <fajrianaidilp@gmail.com>
2021-01-12 10:26:02 +0800

    update : update versionName & versionNumber to 4.2.6

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -287,7 +287,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Text(
-                      'v4.2.5',
+                      'v4.2.6',
                       style: TextStyle(
                           color: Colors.grey,
                           fontSize: 12.0,
