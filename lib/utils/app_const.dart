ed1f330973aa8d7383ef4501131832ebfabc798f
ryanaidilp <fajrianaidilp@gmail.com>
2021-01-28 04:08:09 +0800

    update : update app_const.dart - fix linting issue - add new const

--- lib/utils/app_const.dart
+++ lib/utils/app_const.dart
@@ -1,123 +1,125 @@
 // Shared Preferences Keys
 
-const String PREFS_TOKEN_KEY = 'token';
-const String PREFS_USER_KEY = 'user';
-const String PREFS_EMPLOYEE_KEY = 'employee';
-const String PREFS_ALARM_KEY = 'alarm';
-const String PREFS_SEEN_KEY = 'seen';
+import 'package:flutter/material.dart';
+
+const String prefsTokenKey = 'token';
+const String prefsUserKey = 'user';
+const String prefsEmployeeKey = 'employee';
+const String prefsAlarmKey = 'alarm';
+const String prefsSeenKey = 'seen';
 
 // General
-const String JSON_DATA_FIELD = 'data';
-const String APPROVAL_STATUS_FIELD = 'approval_status';
+const String jsonDataField = 'data';
+const String approvalStatusField = 'approval_status';
 
 // User/Employee
-const String USER_ID_FIELD = 'id';
-const String USER_NIP_FIELD = 'nip';
-const String USER_NAME_FIELD = 'name';
-const String USER_PHONE_FIELD = 'phone';
-const String USER_GENDER_FIELD = 'gender';
-const String USER_DEPARTMENT_FIELD = 'department';
-const String USER_STATUS_FIELD = 'status';
-const String USER_POSITION_FIELD = 'position';
-const String USER_UNREAD_NOTIFICATION_COUNT_FIELD = 'unread_notifications';
-const String USER_HOLIDAY_FIELD = 'holiday';
-const String USER_IS_WEEKEND_FIELD = 'is_weekend';
-const String USER_TOKEN_FIELD = 'token';
-const String USER_NEXT_PRESENCE_FIELD = 'next_presence';
-const String USER_PRESENCES_FIELD = 'presence';
-const String USER_RANK_FIELD = 'rank';
-const String USER_GROUP_FIELD = 'group';
+const String userIdField = 'id';
+const String userNipField = 'nip';
+const String userNameField = 'name';
+const String userPhoneField = 'phone';
+const String userGenderField = 'gender';
+const String userDepartmentField = 'department';
+const String userStatusField = 'status';
+const String userPositionField = 'position';
+const String userUnreadNotificationsCountField = 'unread_notifications';
+const String userIsHolidayField = 'holiday';
+const String userIsWeekendField = 'is_weekend';
+const String userTokenField = 'token';
+const String userNextPresenceField = 'next_presence';
+const String userPresencesField = 'presence';
+const String userRankField = 'rank';
+const String userGroupField = 'group';
 
 // Presence
-const String PRESENCE_DATE_FIELD = 'date';
-const String PRESENCE_CODE_TYPE_FIELD = 'code_type';
-const String PRESENCE_STATUS_FIELD = 'status';
-const String PRESENCE_ATTEND_TIME_FIELD = 'attend_time';
-const String PRESENCE_LOCATION_FIELD = 'location';
-const String PRESENCE_PHOTO_FIELD = 'photo';
-const String PRESENCE_START_TIME_FIELD = 'start_time';
-const String PRESENCE_END_TIME_FIELD = 'end_time';
+const String presenceDateField = 'date';
+const String presenceCodeTypeField = 'code_type';
+const String presenceStatusField = 'status';
+const String presenceAttendTimeField = 'attend_time';
+const String presenceLocationField = 'location';
+const String presencePhotoField = 'photo';
+const String presenceStartTimeField = 'start_time';
+const String presenceEndTimeField = 'end_time';
 
 // Outstation/Permission
-const String OUTSTATION_ID_FIELD = 'id';
-const String OUTSTATION_TITLE_FIELD = 'title';
-const String OUTSTATION_DESCRIPTION_FIELD = 'description';
-const String OUTSTATION_IS_APPROVED_FIELD = 'is_approved';
-const String OUTSTATION_PHOTO_FIELD = 'photo';
-const String OUTSTATION_DUE_DATE_FIELD = 'due_date';
-const String OUTSTATION_START_DATE_FIELD = 'start_date';
-const String OUTSTATION_USER_FIELD = 'user';
+const String outstationIdField = 'id';
+const String outstationTitleField = 'title';
+const String outstationDescriptionField = 'description';
+const String outstationIsApprovedField = 'is_approved';
+const String outstationPhotoField = 'photo';
+const String outstationDueDateField = 'due_date';
+const String outstationStartDateField = 'start_date';
+const String outstationUserField = 'user';
 
 // Paid Leave
-const String PAID_LEAVE_ID_FIELD = 'id';
-const String PAID_LEAVE_TITLE_FIELD = 'title';
-const String PAID_LEAVE_CATEGORY_FIELD = 'category';
-const String PAID_LEAVE_DESCRIPTION_FIELD = 'description';
-const String PAID_LEAVE_IS_APPROVED_FIELD = 'is_approved';
-const String PAID_LEAVE_PHOTO_FIELD = 'photo';
-const String PAID_LEAVE_DUE_DATE_FIELD = 'due_date';
-const String PAID_LEAVE_START_DATE_FIELD = 'start_date';
-const String PAID_LEAVE_USER_FIELD = 'user';
+const String paidLeaveIdField = 'id';
+const String paidLeaveTitleField = 'title';
+const String paidLeaveCategoryField = 'category';
+const String paidLeaveDescriptionField = 'description';
+const String paidLeaveIsApprovedField = 'is_approved';
+const String paidLeavePhotoField = 'photo';
+const String paidLeaveDueDateField = 'due_date';
+const String paidLeaveStartDateField = 'start_date';
+const String paidLeaveUserField = 'user';
 
 // Absent Permission
-const String ABSENT_PERMISSION_ID_FIELD = 'id';
-const String ABSENT_PERMISSION_TITLE_FIELD = 'title';
-const String ABSENT_PERMISSION_DESCRIPTION_FIELD = 'description';
-const String ABSENT_PERMISSION_IS_APPROVED_FIELD = 'is_approved';
-const String ABSENT_PERMISSION_PHOTO_FIELD = 'photo';
-const String ABSENT_PERMISSION_DUE_DATE_FIELD = 'due_date';
-const String ABSENT_PERMISSION_START_DATE_FIELD = 'start_date';
-const String ABSENT_PERMISSION_USER_FIELD = 'user';
+const String absentPermissionIdField = 'id';
+const String absentPermissionTitleField = 'title';
+const String absentPermissionDescriptionField = 'description';
+const String absentPermissionIsApprovedField = 'is_approved';
+const String absentPermissionPhotoField = 'photo';
+const String absentPermissionDueDateField = 'due_date';
+const String absentPermissionStartDateField = 'start_date';
+const String absentPermissionUserField = 'user';
 
 // Notification
-const String NOTIFICATION_ID_FIELD = 'id';
-const String NOTIFICATION_NOTIFIABLE_ID_FIELD = 'notifiable_id';
-const String NOTIFICATION_NOTIFIABLE_TYPE_FIELD = 'notifiable_type';
-const String NOTIFICATION_IS_READ_FIELD = 'is_read';
+const String notificationIdField = 'id';
+const String notificationNotifiableIdField = 'notifiable_id';
+const String notificationNotifiableTypeField = 'notifiable_type';
+const String notificationIsReadField = 'is_read';
 
 // Location
-const String LOCATION_LATITUDE_FIELD = 'latitude';
-const String LOCATION_LONGITUDE_FIELD = 'longitude';
-const String LOCATION_ADDRESS_FIELD = 'address';
+const String locationLatitudeField = 'latitude';
+const String locationLongitudeField = 'longitude';
+const String locationAddressField = 'address';
 
 // Absent Report
-const String REPORT_ATTENDANCE_PERCENTAGE_FIELD = 'attendance_percentage';
-const String REPORT_DAY_FIELD = 'day';
-const String REPORT_LIMIT_FIELD = 'limit';
-const String REPORT_LATE_COUNT_FIELD = 'late_count';
-const String REPORT_LEAVE_EARLY_COUNT_FIELD = 'leave_early_count';
-const String REPORT_NOT_MORNING_PARADE_COUNT_FIELD = 'not_morning_parade_count';
-const String REPORT_EARLY_LUNCH_BREAK_COUNT_FIELD = 'early_lunch_break_count';
-const String REPORT_NOT_COME_AFTER_LUNCH_BREAK_COUNT_FIELD =
+const String reportAttendancePercentageFieldField = 'attendance_percentage';
+const String reportDayField = 'day';
+const String reportLimitField = 'limit';
+const String reportLateCountField = 'late_count';
+const String reportLeaveEarlyFieldCountField = 'leave_early_count';
+const String reportNotMorningParadeCountField = 'not_morning_parade_count';
+const String reportEarlyLunchBreakCountField = 'early_lunch_break_count';
+const String reportNotComeAfterLunchBreakCountField =
     'not_come_after_lunch_break_count';
-const String REPORT_TOTAL_WORK_DAY_FIELD = 'total_work_day';
-const String REPORT_ANNUAL_LEAVE_FIELD = 'annual_leave';
-const String REPORT_IMPORTANT_REASON_LEAVE_FIELD = 'important_reason_leave';
-const String REPORT_SICK_LEAVE_FIELD = 'sick_leave';
-const String REPORT_MATERNITY_LEAVE_FIELD = 'maternity_leave';
-const String REPORT_OUT_OF_LIABILITY_LEAVE_FIELD = 'out_of_liability_leave';
-const String REPORT_PERCENTAGE_FIELD = 'percentage';
-const String ABSENT_REPORT_YEARLY_FIELD = 'yearly';
-const String ABSENT_REPORT_MONTHLY_FIELD = 'monthly';
-const String ABSENT_REPORT_DAILY_FIELD = 'daily';
-const String ABSENT_REPORT_HOLIDAYS_FIELD = 'holidays';
+const String reportTotalWorkDayField = 'total_work_day';
+const String reportAnnualLeaveField = 'annual_leave';
+const String reportImportantReasonLeaveField = 'important_reason_leave';
+const String reportSickLeaveField = 'sick_leave';
+const String reportMaternityLeaveField = 'maternity_leave';
+const String reportOutOfLiabilityLeaveField = 'out_of_liability_leave';
+const String absentReportPercentageField = 'percentage';
+const String absentReportYearlyField = 'yearly';
+const String absentReportMonthlyField = 'monthly';
+const String absentReportDailyField = 'daily';
+const String absentReportHolidaysField = 'holidays';
 
 // DailyData (Report)
-const String DAILY_DATE_FIELD = 'date';
-const String DAILY_PRESENCES_FIELD = 'attendances';
-const String DAILY_DATA_ATTEND_TIME_FIELD = 'attend_time';
-const String DAILY_DATA_ATTEND_TYPE_FIELD = 'absent_type';
-const String DAILY_DATA_ATTEND_STATUS_FIELD = 'attend_status';
+const String dailyDateField = 'date';
+const String dailyPresencesField = 'attendances';
+const String dailyDataAttendTimeField = 'attend_time';
+const String dailyDataAttendTypeField = 'absent_type';
+const String dailyDataAttendStatusField = 'attend_status';
 
 // Yearly
-const String YEARLY_ABSENT_FIELD = 'absent';
-const String YEARLY_ABSENT_PERMISSION_FIELD = 'absent_permission';
-const String YEARLY_OUTSTATION_FIELD = 'outstation';
+const String yearlyAbsentField = 'absent';
+const String yearlyAbsentPermissionField = 'absent_permission';
+const String yearlyOutstationField = 'outstation';
 
 // Holiday
-const String HOLIDAY_DATE_FIELD = 'date';
-const String HOLIDAY_NAME_FIELD = 'name';
-const String HOLIDAY_DESCRIPTION_FIELD = 'description';
+const String holidayDateField = 'date';
+const String holidayNameField = 'name';
+const String holidayDescriptionField = 'description';
 
 const Map<String, dynamic> paidLeaveCategories = {
   'Cuti Tahunan (97.5%)': 1,
@@ -126,3 +128,30 @@ const Map<String, dynamic> paidLeaveCategories = {
   'Cuti Sakit (97.5%)': 4,
   'Cuti Diluar Tanggungan (0%)': 5
 };
+
+const sizedBox = SizedBox();
+const sizedBoxW2 = SizedBox(width: 2);
+const sizedBoxW4 = SizedBox(width: 4);
+const sizedBoxW5 = SizedBox(width: 5);
+const sizedBoxW6 = SizedBox(width: 6);
+const sizedBoxW8 = SizedBox(width: 8);
+const sizedBoxW10 = SizedBox(width: 8);
+const sizedBoxW16 = SizedBox(width: 16);
+const sizedBoxW20 = SizedBox(width: 20);
+const sizedBoxW30 = SizedBox(width: 30);
+const sizedBoxH2 = SizedBox(height: 2);
+const sizedBoxH4 = SizedBox(height: 4);
+const sizedBoxH5 = SizedBox(height: 5);
+const sizedBoxH6 = SizedBox(height: 6);
+const sizedBoxH8 = SizedBox(height: 8);
+const sizedBoxH10 = SizedBox(height: 8);
+const sizedBoxH16 = SizedBox(height: 16);
+const sizedBoxH20 = SizedBox(height: 20);
+const sizedBoxH30 = SizedBox(height: 30);
+
+const dividerT1 = Divider(thickness: 1);
+const dividerT2 = Divider(thickness: 2);
+const verticalDiv = VerticalDivider(
+  thickness: 1,
+  color: Colors.black,
+);
