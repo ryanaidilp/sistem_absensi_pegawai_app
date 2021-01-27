9b3f67be0c7bfd8143125871608711b8b6839006
ryanaidilp <fajrianaidilp@gmail.com>
2021-01-28 04:08:42 +0800

    update : update all models - implements linting in all models

--- lib/models/outstation.dart
+++ lib/models/outstation.dart
@@ -25,16 +25,16 @@ class Outstation {
 
   factory Outstation.fromJson(Map<String, dynamic> json) {
     return Outstation(
-        id: json[OUTSTATION_ID_FIELD] as int,
-        title: json[OUTSTATION_TITLE_FIELD] as String,
-        description: json[OUTSTATION_DESCRIPTION_FIELD] as String,
-        isApproved: json[OUTSTATION_IS_APPROVED_FIELD] as bool,
-        photo: json[OUTSTATION_PHOTO_FIELD] as String,
-        approvalStatus: json[APPROVAL_STATUS_FIELD] as String,
-        dueDate: DateTime.parse(json[OUTSTATION_DUE_DATE_FIELD]),
-        startDate: DateTime.parse(json[OUTSTATION_START_DATE_FIELD]),
-        user: json[OUTSTATION_USER_FIELD] != null
-            ? User.fromJson(json[OUTSTATION_USER_FIELD])
+        id: json[outstationIdField] as int,
+        title: json[outstationTitleField] as String,
+        description: json[outstationDescriptionField] as String,
+        isApproved: json[outstationIsApprovedField] as bool,
+        photo: json[outstationPhotoField] as String,
+        approvalStatus: json[approvalStatusField] as String,
+        dueDate: DateTime.parse(json[outstationDueDateField].toString()),
+        startDate: DateTime.parse(json[outstationStartDateField].toString()),
+        user: json[outstationUserField] != null
+            ? User.fromJson(json[outstationUserField] as Map<String, dynamic>)
             : null);
   }
 }
