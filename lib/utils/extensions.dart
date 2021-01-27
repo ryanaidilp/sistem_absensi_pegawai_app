9981c385755bf3aeda0113f9090fb938fe36d13d
ryanaidilp <fajrianaidilp@gmail.com>
2021-01-28 04:11:04 +0800

    update : implements linting in extensions.dart and file_util.dart

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -1,7 +1,5 @@
 extension DateOnlyCompare on DateTime {
   bool isSameDate(DateTime other) {
-    return this.year == other.year &&
-        this.month == other.month &&
-        this.day == other.day;
+    return year == other.year && month == other.month && day == other.day;
   }
 }
