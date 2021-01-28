dca4bb6b9a7000e7553add386a96c46773ae2090
ryanaidilp <fajrianaidilp@gmail.com>
2021-01-28 19:35:10 +0800

    update : update permission_list_screen.dart - fetch data by month and year to reduce memory usage

--- lib/screen/permission_list_screen.dart
+++ lib/screen/permission_list_screen.dart
@@ -1,12 +1,15 @@
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_cupertino_date_picker/flutter_cupertino_date_picker.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:spo_balaesang/models/absent_permission.dart';
 import 'package:spo_balaesang/repositories/data_repository.dart';
 import 'package:spo_balaesang/screen/change_absent_permission_photo_screen.dart';
 import 'package:spo_balaesang/screen/create_permission_screen.dart';
+import 'package:spo_balaesang/utils/app_const.dart';
 import 'package:spo_balaesang/utils/view_util.dart';
 import 'package:spo_balaesang/widgets/employee_proposal_widget.dart';
 
@@ -18,6 +21,7 @@ class PermissionListScreen extends StatefulWidget {
 class _PermissionListScreenState extends State<PermissionListScreen> {
   List<AbsentPermission> _permissions = <AbsentPermission>[];
   bool _isLoading = false;
+  DateTime _date;
 
   @override
   void setState(void Function() fn) {
@@ -32,7 +36,8 @@ class _PermissionListScreenState extends State<PermissionListScreen> {
         _isLoading = true;
       });
       final dataRepo = Provider.of<DataRepository>(context, listen: false);
-      final Map<String, dynamic> _result = await dataRepo.getAllPermissions();
+      final Map<String, dynamic> _result =
+          await dataRepo.getAllPermissions(_date);
       final List<dynamic> permissions = _result['data'] as List<dynamic>;
       final List<AbsentPermission> _data = permissions
           .map(
@@ -55,57 +60,73 @@ class _PermissionListScreenState extends State<PermissionListScreen> {
   @override
   void initState() {
     super.initState();
+    _date = DateTime.now();
     _fetchPermissionData();
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    DatePicker.showDatePicker(context,
+        initialDateTime: _date,
+        minDateTime: DateTime(2021),
+        maxDateTime: DateTime(DateTime.now().year + 5), onConfirm: (picked, _) {
+      if (picked != null) {
+        setState(() {
+          _date = picked;
+        });
+        _fetchPermissionData();
+      }
+    }, locale: DateTimePickerLocale.id, dateFormat: 'MMMM-y');
   }
 
   Widget _buildBody() {
     if (_isLoading) {
-      return const Center(
-          child: SpinKitFadingFour(
-        size: 45,
-        color: Colors.blueAccent,
-      ));
-    }
-
-    if (_permissions.isEmpty) {
-      return Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                width: Get.width * 0.5,
-                height: Get.height * 0.3,
-                child: const FlareActor(
-                  'assets/flare/not_found.flr',
-                  animation: 'empty',
-                ),
-              ),
-              const Text('Belum ada izin yang diajukan!')
-            ]),
+      return SizedBox(
+        height: Get.height * 0.7,
+        child: const Center(
+            child: SpinKitFadingFour(
+          size: 45,
+          color: Colors.blueAccent,
+        )),
       );
     }
 
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ListView.builder(
-        itemBuilder: (_, index) {
-          final AbsentPermission permission = _permissions[index];
-          final DateTime dueDate = _permissions[index].dueDate;
-          final DateTime startDate = _permissions[index].startDate;
-          return EmployeeProposalWidget(
-            photo: permission.photo,
-            heroTag: permission.id.toString(),
-            isApproved: permission.isApproved,
-            startDate: startDate,
-            dueDate: dueDate,
-            approvalStatus: permission.approvalStatus,
-            description: permission.description,
-            title: permission.title,
-            updateWidget: ChangePermissionPhotoScreen(permission: permission),
-          );
-        },
-        itemCount: _permissions.length,
-      ),
+    if (_permissions.isEmpty) {
+      return SizedBox(
+        height: Get.height * 0.7,
+        child: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  width: Get.width * 0.5,
+                  height: Get.height * 0.3,
+                  child: const FlareActor(
+                    'assets/flare/not_found.flr',
+                    animation: 'empty',
+                  ),
+                ),
+                const Text('Belum ada izin yang diajukan!')
+              ]),
+        ),
+      );
+    }
+
+    return Column(
+      children: _permissions.map((AbsentPermission permission) {
+        final DateTime dueDate = permission.dueDate;
+        final DateTime startDate = permission.startDate;
+        return EmployeeProposalWidget(
+          photo: permission.photo,
+          heroTag: permission.id.toString(),
+          isApproved: permission.isApproved,
+          startDate: startDate,
+          dueDate: dueDate,
+          approvalStatus: permission.approvalStatus,
+          description: permission.description,
+          title: permission.title,
+          updateWidget: ChangePermissionPhotoScreen(permission: permission),
+        );
+      }).toList(),
     );
   }
 
@@ -123,6 +144,43 @@ class _PermissionListScreenState extends State<PermissionListScreen> {
           },
           child: const Icon(Icons.add),
         ),
-        body: _buildBody());
+        body: Container(
+          height: Get.height,
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  'Pilih Tahun & Bulan : ',
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      DateFormat.yMMMM('id_ID').format(_date),
+                      style: const TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    IconButton(
+                        icon: Icon(
+                          Icons.calendar_today_rounded,
+                          color: Colors.blueAccent[400],
+                        ),
+                        onPressed: () {
+                          _selectDate(context);
+                        })
+                  ],
+                ),
+                sizedBoxH4,
+                dividerT1,
+                sizedBoxH4,
+                _buildBody()
+              ],
+            ),
+          ),
+        ));
   }
 }
