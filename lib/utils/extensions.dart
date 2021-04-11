f54eb81373afef56caea9110d56e481d443155b9
ryanaidilp <fajrianaidilp@gmail.com>
2021-04-12 00:58:39 +0800

    chore : add isToday and isWeekend extensions for datetime

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -2,4 +2,14 @@ extension DateOnlyCompare on DateTime {
   bool isSameDate(DateTime other) {
     return year == other.year && month == other.month && day == other.day;
   }
+
+  bool isToday() {
+    final now = DateTime.now();
+    final diff = now.difference(this).inDays;
+    return diff == 0 && now.day == day;
+  }
+
+  bool isWeekend() {
+    return weekday == 6 || weekday == 7;
+  }
 }
